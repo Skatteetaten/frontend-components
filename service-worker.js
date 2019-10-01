b95369aaf6c1a47f5caa43937be8c53b1e9eecc8
Espen Strømland <espen.stromland@skatteetaten.no>
2019-10-01 12:13:01 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.bff3ef3add80ac8ef7c70ac6b45eced6.js"
+  "precache-manifest.f187355b279d3dc27e1bed42fbfdf771.js"
 );
 
 self.addEventListener('message', (event) => {
