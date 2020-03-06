25e173fd81892b72598ee9de995dead5141d602d
Eivind Løland-Andersen <eivind.loland-andersen@skatteetaten.no>
2020-03-06 12:15:00 +0100

    Run lint

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -77,7 +77,7 @@ export default class Dialog extends React.PureComponent<
           modalProps={{
             isBlocking,
             className: classnames(styles.main, className),
-              ...modalProps
+            ...modalProps
           }}
         >
           {isCalloutVisible && (
