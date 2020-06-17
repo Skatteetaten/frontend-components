d9804d21c01ef8ebcda1dec56976647d194f3617
Espen Myklevoll <Espen.Myklevoll@skatteetaten.no>
2020-06-17 14:27:50 +0200

    FRONT-486 - bygg

--- src/components/MessageBar/MessageBar.tsx
+++ src/components/MessageBar/MessageBar.tsx
@@ -8,6 +8,7 @@ import * as React from 'react';
 // @ts-ignore TODO
 import { CSSTransition } from 'react-transition-group';
 import { default as MessageBarButton } from '../Button/Button';
+
 import { getClassNames } from './MessageBar.classNames';
 
 export interface MessageBarProps extends IMessageBarProps {
