373b00bd14ef19b06db45b93882a518d9b27ed53
k25262 <malin.milford@skatteetaten.no>
2021-06-15 10:55:07 +0200

    test

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -102,6 +102,7 @@ const SumRow = (props: {
     ? props.columns.length - props.sum.colspan - 1
     : 0;
   const emptyCells = Array.from(Array(columns).keys());
+  let counter = 0;
   return (
     <tr>
       <th colSpan={props.sum.colspan} scope="row" className={'sum'}>
@@ -110,7 +111,7 @@ const SumRow = (props: {
       <td className={'sum'}>{props.sum.total}</td>
       {props.editableRows && <td />}
       {props.expandableRows && props.expandIconPlacement === 'after' && <td />}
-      {!!emptyCells.length && emptyCells.map(() => <td />)}
+      {!!emptyCells.length && emptyCells.map(() => <td key={counter++} />)}
     </tr>
   );
 };
