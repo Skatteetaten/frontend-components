bf3ec8a85277c6cea425399a876d7f1ac3a3ce60
Marion Le Pontois <marion.lepontois@skatteetaten.no>
2021-08-26 10:30:27 +0200

    oppgraderer til react 17 og oppdaterer dependencies

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,8 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import React from 'react';
-React.useLayoutEffect = React.useEffect;
 import './components/utils/loadTheme';
+React.useLayoutEffect = React.useEffect;
 jest.mock('./components/utils/generateId');
 
 configure({ adapter: new Adapter() });
