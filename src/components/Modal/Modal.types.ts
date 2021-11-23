8ecf6d77cb05ab742acece27adfbaf50d0b928e8
Berg, Johannes <Johannes.Berg@skatteetaten.no>
2021-11-23 09:57:42 +0100

    Applied suggestion

--- src/components/Modal/Modal.types.ts
+++ src/components/Modal/Modal.types.ts
@@ -3,7 +3,7 @@ import * as React from 'react';
 export interface ModalProps {
   /** Unikt navn til modal-instancen - refereres ved open/close osv */
   name: string;
-  /** Mulighet til å angi shadowRoot til en custom node Modal'en skal instansieres i. Dersom den inneholder en id="style-injector" vi modal-wrapper være nested der */
+  /** Mulighet til å angi shadowRoot til en custom node Modal'en skal instansieres i. Dersom den inneholder en id="style-injector" vil modal-wrapper være nested der */
   shadowRootNode?: Document | ShadowRoot;
   /** Skjuler lukkeknappen */
   hideCloseButton?: boolean;
