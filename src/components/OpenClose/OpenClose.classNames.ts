4e50e5b3c7eb1e4be05503a76770758f0bac6262
Eivind TMO4bGFuZC1BbmRlcnNlbg== <Eivind.Loland-Andersen@skatteetaten.no>
2021-12-02 16:29:40 +0100

    Designtoken colors

--- src/components/OpenClose/OpenClose.classNames.ts
+++ src/components/OpenClose/OpenClose.classNames.ts
@@ -1,12 +1,10 @@
 import { mergeStyleSets } from '@fluentui/merge-styles';
-import { getTheme } from '@fluentui/react/lib/Styling';
-import { PaletteProps } from '../utils';
 
-import spacing from '../utils/designtokens/_spacing.json';
-import fonts from '../utils/designtokens/_fontSizes.json';
+import designtokenSpacing from '../utils/designtokens/_spacing.json';
+import designtokenFonts from '../utils/designtokens/_fontSizes.json';
+import designtokenColors from '../utils/designtokens/_colors.json';
 
 export function getClassNames(props) {
-  const palette = getTheme().palette as PaletteProps;
   const { compact, iconRight, underline } = props;
 
   return mergeStyleSets({
@@ -16,35 +14,37 @@ export function getClassNames(props) {
       justifyContent: 'space-between',
       alignItems: 'flex-start',
       border: 'none',
-      color: palette.skeColor.blue,
-      fontSize: compact ? fonts['ske-font-size-s'] : fonts['ske-font-size-l'],
-      fontWeight: fonts['ske-font-weight-bold'],
+      color: designtokenColors['ske-color-interactive'],
+      fontSize: compact
+        ? designtokenFonts['ske-font-size-s']
+        : designtokenFonts['ske-font-size-l'],
+      fontWeight: designtokenFonts['ske-font-weight-bold'],
       padding: compact
-        ? spacing['ske-spacing-xs'] +
+        ? designtokenSpacing['ske-spacing-xs'] +
           ' ' +
-          spacing['ske-spacing-sm'] +
+          designtokenSpacing['ske-spacing-sm'] +
           ' ' +
-          spacing['ske-spacing-xs'] +
+          designtokenSpacing['ske-spacing-xs'] +
           ' ' +
-          spacing['kse-spacing-sm']
-        : spacing['ske-spacing-sm'] +
+          designtokenSpacing['kse-spacing-sm']
+        : designtokenSpacing['ske-spacing-sm'] +
           ' ' +
-          spacing['ske-spacing-md'] +
+          designtokenSpacing['ske-spacing-md'] +
           ' ' +
-          spacing['ske-spacing-sm'] +
+          designtokenSpacing['ske-spacing-sm'] +
           ' ' +
-          spacing['ske-spacing-md'],
+          designtokenSpacing['ske-spacing-md'],
       background: 'none',
-      marginLeft: `-${spacing['ske-spacing-md']}`,
-      paddingLeft: spacing['ske-spacing-md'],
-      marginRight: `-${spacing['ske-spacing-md']}`,
-      paddingRight: spacing['ske-spacing-md'],
+      marginLeft: `-${designtokenSpacing['ske-spacing-md']}`,
+      paddingLeft: designtokenSpacing['ske-spacing-md'],
+      marginRight: `-${designtokenSpacing['ske-spacing-md']}`,
+      paddingRight: designtokenSpacing['ske-spacing-md'],
       cursor: 'pointer',
-      borderBottom: `${spacing['ske-spacing-xs']} solid transparent`,
+      borderBottom: `${designtokenSpacing['ske-spacing-xs']} solid transparent`,
       selectors: {
         '&:hover, &:focus': {
-          background: palette.skeColor.lightBlue,
-          borderBottom: `${spacing['ske-spacing-xs']} solid ${palette.skeColor.interactive}`,
+          background: designtokenColors['ske-color-interactive-light'],
+          borderBottom: `${designtokenSpacing['ske-spacing-xs']} solid ${designtokenColors['ske-color-interactive']}`,
           span: {
             textDecoration: 'none',
           },
@@ -54,39 +54,43 @@ export function getClassNames(props) {
         },
         '& h1, h2, h3, h4, h5, h6': {
           fontSize: compact
-            ? fonts['ske-font-size-s']
-            : fonts['ske-font-size-l'],
+            ? designtokenFonts['ske-font-size-s']
+            : designtokenFonts['ske-font-size-l'],
           margin: '0',
         },
         '& i': {
           transition: '.2s',
-          padding: compact ? spacing['ske-spacing-xs'] + ' 0 0 0' : '0',
-          marginLeft: iconRight ? spacing['ske-spacing-sm'] : '0',
+          padding: compact
+            ? designtokenSpacing['ske-spacing-xs'] + ' 0 0 0'
+            : '0',
+          marginLeft: iconRight ? designtokenSpacing['ske-spacing-sm'] : '0',
           marginTop: compact
-            ? spacing['ske-spacing-xs']
-            : spacing['ske-spacing-sm'],
+            ? designtokenSpacing['ske-spacing-xs']
+            : designtokenSpacing['ske-spacing-sm'],
           fontSize: compact
-            ? fonts['ske-font-size-icon-l']
-            : fonts['ske-font-size-icon-xl'],
+            ? designtokenFonts['ske-font-size-icon-l']
+            : designtokenFonts['ske-font-size-icon-xl'],
         },
       },
     },
     content: {
-      padding: spacing['ske-spacing-md'],
+      padding: designtokenSpacing['ske-spacing-md'],
       marginLeft: compact
-        ? spacing['ske-spacing-lg']
-        : spacing['ske-spacing-xl'],
-      marginBottom: spacing['ske-spacing-md'],
+        ? designtokenSpacing['ske-spacing-lg']
+        : designtokenSpacing['ske-spacing-xl'],
+      marginBottom: designtokenSpacing['ske-spacing-md'],
     },
     contentWhenIconRight: {
-      padding: `${spacing['ske-spacing-md']} 0`,
+      padding: `${designtokenSpacing['ske-spacing-md']} 0`,
       marginLeft: compact
-        ? spacing['ske-spacing-lg']
-        : spacing['ske-spacing-xl'],
-      marginBottom: spacing['ske-spacing-md'],
+        ? designtokenSpacing['ske-spacing-lg']
+        : designtokenSpacing['ske-spacing-xl'],
+      marginBottom: designtokenSpacing['ske-spacing-md'],
     },
     heading: {
-      fontSize: compact ? fonts['ske-font-size-s'] : fonts['ske-font-size-l'],
+      fontSize: compact
+        ? designtokenFonts['ske-font-size-s']
+        : designtokenFonts['ske-font-size-l'],
       margin: '0',
     },
     toggleTitleSpan: {
@@ -94,13 +98,13 @@ export function getClassNames(props) {
       textDecorationColor: 'rgba(19, 98, 174, 0.4)',
       textUnderlineOffset: '0.3rem',
       textDecorationThickness: '0.125rem',
-      lineHeight: fonts['ske-line-height-l'],
+      lineHeight: designtokenFonts['ske-line-height-l'],
       textAlign: 'left',
     },
     toggleTitleLeft: {
       paddingLeft: compact
-        ? spacing['ske-spacing-sm']
-        : spacing['ske-spacing-md'],
+        ? designtokenSpacing['ske-spacing-sm']
+        : designtokenSpacing['ske-spacing-md'],
     },
     toggleButtonOpen: {
       selectors: {
