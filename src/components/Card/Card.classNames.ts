f459f91d3396f5082ff8a80cbc48d8ef4c44ed0b
Espen Myklevoll <Espen.Myklevoll@skatteetaten.no>
2022-02-18 11:20:48 +0100

    FRONT-754 - fjern utkommentert import

--- src/components/Card/Card.classNames.ts
+++ src/components/Card/Card.classNames.ts
@@ -1,6 +1,5 @@
 import { FontSizes, FontWeights, PaletteProps } from '../utils';
 import { CardProps, CardState, CardColor, CardBorder } from './Card.types';
-//import { getTheme } from '@fluentui/react';
 import { getTheme } from '@fluentui/react';
 import { keyframes, mergeStyleSets } from '@fluentui/merge-styles';
 
