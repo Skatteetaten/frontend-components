8f18a7d03de614347a2e2db4ad80a3286132ec14
Eivind TMO4bGFuZC1BbmRlcnNlbg== <Eivind.Loland-Andersen@skatteetaten.no>
2022-04-05 12:55:22 +0200

    Added test for language

--- src/components/TopBanner/TopBanner.spec.js
+++ src/components/TopBanner/TopBanner.spec.js
@@ -47,6 +47,49 @@ describe('TopBanner komponent', () => {
     expect(wrapper.find('ImageBase').prop('height')).toEqual(68);
   });
 
+  it('rendrer TopBanner med engelsk logo og link ', () => {
+    const wrapper = oppsettFullDOM({
+      external: true,
+      homeText: 'Til skatteetaten.no',
+      title: 'Ekstern publikumsløsning',
+      logoLink: true,
+      language: 'en',
+    });
+
+    expect(wrapper.find('h1').html()).toContain('Ekstern publikumsløsning');
+    expect(wrapper.html()).toContain('contentWrapper');
+    expect(wrapper.html()).toContain('headerMain');
+    expect(wrapper.find('.ms-Icon').prop('data-icon-name')).toEqual(
+      'ArrowBack'
+    );
+    expect(wrapper.find('ImageBase').prop('height')).toEqual(68);
+    expect(wrapper.find('a').prop('href')).toEqual(
+      'https://www.skatteetaten.no'
+    );
+    expect(wrapper.find('img').prop('alt')).toEqual(
+      'Front page The Norwegian Tax Administration'
+    );
+  });
+
+  it('rendrer TopBanner med samisk alt-tekst i logo ', () => {
+    const wrapper = oppsettFullDOM({
+      external: true,
+      homeText: 'Til skatteetaten.no',
+      title: 'Ekstern publikumsløsning',
+      logoLink: false,
+      language: 'se',
+    });
+
+    expect(wrapper.find('h1').html()).toContain('Ekstern publikumsløsning');
+    expect(wrapper.html()).toContain('contentWrapper');
+    expect(wrapper.html()).toContain('headerMain');
+    expect(wrapper.find('.ms-Icon').prop('data-icon-name')).toEqual(
+      'ArrowBack'
+    );
+    expect(wrapper.find('ImageBase').prop('height')).toEqual(68);
+    expect(wrapper.find('img').prop('alt')).toEqual('Bálkádoarjja dovdomearka');
+  });
+
   it('rendrer TopBanner med kompakt visning for eksterne arbeidsflater ', () => {
     const wrapper = oppsettFullDOM({
       external: true,
