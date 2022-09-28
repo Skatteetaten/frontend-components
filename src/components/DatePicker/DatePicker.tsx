b50b595e66f0530076b0a0fabce99fa668e6b26c
Ann Magdalen Hegge <annmagdalen.hegge@skatteetaten.no>
2022-09-28 10:29:21 +0200

    Flytt hjelpefunksjoner fra Datepicker til egen utils-fil

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -13,58 +13,12 @@ import { LabelWithCallout } from '../LabelWithCallout';
 import { getClassNames, getCalendarClassNames } from './DatePicker.classNames';
 import { DatePickerProps } from './DatePicker.types';
 import { ErrorMessage } from '../ErrorMessage';
-
-const DEFAULT_DATE_FORMAT = 'DD.MM.YYYY';
-const DEFAULTFORMATDATE = (date: Date | null | undefined): string => {
-  return date
-    ? Intl.DateTimeFormat('no', {
-        year: 'numeric',
-        month: '2-digit',
-        day: 'numeric',
-      }).format(date)
-    : '';
-};
-
-const DEFAULTPARSEDATEFROMSTRING = (date: string): Date | string => {
-  try {
-    if (date && date.length === 10) {
-      const day: number = parseInt(date.substring(0, 2));
-      const month: number = parseInt(date.substring(3, 5));
-      const year: number = parseInt(date.substring(6, 10));
-      const formattedDate: Date = new Date(year, month - 1, day);
-      return formattedDate;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-  return '';
-};
-
-const monthsForLocale = (
-  localeName = 'no',
-  monthFormat: 'long' | 'short' = 'long'
-) => {
-  const format = new Intl.DateTimeFormat(localeName, { month: monthFormat })
-    .format;
-  const months: string[] = [];
-  for (let month = 0; month < 12; month++) {
-    months.push(format(new Date(Date.UTC(2020, month))));
-  }
-  return months;
-};
-
-const weekdaysForLocale = (
-  localeName = 'no',
-  dayFormat: 'long' | 'short' = 'long'
-) => {
-  const format = new Intl.DateTimeFormat(localeName, { weekday: dayFormat })
-    .format;
-  const weekdays: string[] = [];
-  for (let day = 0; day < 7; day++) {
-    weekdays.push(format(new Date(Date.UTC(2020, 7, day + 2)))); // add 2 days to let the week start on Sunday
-  }
-  return weekdays;
-};
+import {
+  DEFAULTFORMATDATE,
+  DEFAULTPARSEDATEFROMSTRING,
+  monthsForLocale,
+  weekdaysForLocale,
+} from './utils';
 
 /*
  * visibleName DatePicker (Datovelger)
@@ -131,6 +85,8 @@ export const DatePicker: React.FC<DatePickerProps> = (
   React.useEffect(() => {
     setReadOnly(readonlyMode && !editMode);
   }, [editMode, readonlyMode]);
+
+  const DEFAULT_DATE_FORMAT = 'DD.MM.YYYY';
 
   const DEFAULT_STRINGS = {
     months: monthsForLocale(language !== 'en' ? 'no' : 'en', 'long'),
