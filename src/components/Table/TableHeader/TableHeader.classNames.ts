a469dae59f20c0fd56253bbb307416d0c20ab791
Olav Hellesø-Knutsen <olav.helleso-knutsen@skatteetaten.no>
2024-04-11 14:59:05 +0200

    FRONT-1278 sr-only. refakrotrert duplikate sr-only stiler

--- src/components/Table/TableHeader/TableHeader.classNames.ts
+++ src/components/Table/TableHeader/TableHeader.classNames.ts
@@ -11,17 +11,6 @@ export const getClassNames = (props) => {
   const palette = getTheme().palette as PaletteProps;
 
   return mergeStyleSets({
-    srOnly: {
-      position: 'absolute',
-      width: '1px',
-      height: '1px',
-      padding: '0',
-      margin: '-1px',
-      overflow: 'hidden',
-      clip: 'rect(0, 0, 0, 0)',
-      whiteSpace: 'nowrap',
-      borderWidth: '0',
-    },
     tabellThead: {
       display: 'table-header-group',
     },
